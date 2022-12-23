fb255932c9baf66acb49149b92bcdd1e58170b66
kradova <ksenia.radova@gmail.com>
2022-12-23 15:34:13 +0300

    + Show month name

--- src/kradova/java/homework3/Main.java
+++ src/kradova/java/homework3/Main.java
@@ -9,6 +9,15 @@ public class Main {
         //Check odd number
         numChecker.checkOddEven(5);
 
+        MonthNameDeclarer monthDeclarer = new MonthNameDeclarer();
+        monthDeclarer.printMonthInWordIfElse(2);
+        monthDeclarer.printMonthInWordIfElse(5);
+        monthDeclarer.printMonthInWordIfElse(7);
+        monthDeclarer.printMonthInWordIfElse(44);
 
+        monthDeclarer.printMonthInWordSwitch(1);
+        monthDeclarer.printMonthInWordSwitch(4);
+        monthDeclarer.printMonthInWordSwitch(6);
+        monthDeclarer.printMonthInWordSwitch(55);
     }
 }
--- src/kradova/java/homework3/PrintMonthInWord.java
+++
@@ -1,2 +0,0 @@
-package homework3;public class PrintMonthInWord {
-}
