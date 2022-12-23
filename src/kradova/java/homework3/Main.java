f554d2acc92b5410752dfebe64f4565d18854d78
kradova <ksenia.radova@gmail.com>
2022-12-23 14:57:27 +0300

    + Check Odd Even Numbers

---
+++ src/kradova/java/homework3/Main.java
@@ -0,0 +1,14 @@
+package homework3;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NumberChecker numChecker = new NumberChecker();
+        //Check even number
+        numChecker.checkOddEven(4);
+        //Check odd number
+        numChecker.checkOddEven(5);
+
+
+    }
+}
---
+++ src/kradova/java/homework3/NumberChecker.java
@@ -0,0 +1,13 @@
+package homework3;
+
+public class NumberChecker {
+
+    public  void checkOddEven(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+        System.out.println("BYE");
+    }
+}
