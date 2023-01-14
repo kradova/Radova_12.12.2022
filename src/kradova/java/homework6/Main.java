f637138cb1925ce3393ea0bf10edc6ece53a3156
kradova <ksenia.radova@gmail.com>
2023-01-14 16:49:03 +0300

    + Employee class inheritor of Person class

--- src/kradova/java/homework6/Main.java
+++ src/kradova/java/homework6/Main.java
@@ -17,5 +17,14 @@ public class Main {
 
         Person person_4 = new Person("Will", "Smith", "New York", "2936729462846");
         System.out.println(person_1.call(person_4));
+
+        Employee employee_1 = new Employee("Sherlock ", "Holmes ", "London", "37742123513", "detective");
+        System.out.println(employee_1.call());
+
+        Person employee_2 = new Employee("Will", "Smith", "New York", "2936729462846", "actor");
+        System.out.println(employee_2.call());
+
+        System.out.println(employee_1.call(employee_2));
+        System.out.println(person_3.call(employee_1));
     }
 }
