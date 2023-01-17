34ac3d3323bca041ca791fd75e8211000928c616
kradova <ksenia.radova@gmail.com>
2023-01-18 01:00:24 +0300

    + homework8 refactoring

--- src/kradova/java/homework8/Faculty.java
+++ src/kradova/java/homework8/Faculty.java
@@ -1,24 +1,24 @@
 package homework8;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Faculty {
-    static private final List<Student> students = new LinkedList<Student>();
+    private static final List<Student> students = new ArrayList<>();
+    private static boolean exist;
 
     public static void printStudents(int course) {
         System.out.println("Course #" + course);
-        boolean exist = false;
-        for (Student student : students) {
-            if (student.getCourse() == course) {
-                System.out.println(student.getName());
-                exist = true;
-            }
-        }
+        exist = false;
+        students.stream().filter(student -> student.getCourse() == course)
+                .forEach(student -> {
+                    System.out.println(student.getName());
+                    exist = true;
+                });
         if (!exist) {
             System.out.println("There are no students in the course #" + course + " or such course does not exist at all");
         }
-    }//printStudents
+    }
 
     public static void addStudent(Student student) {
         students.add(student);
@@ -27,4 +27,4 @@ public class Faculty {
     public static void removeStudent(Student student) {
         students.remove(student);
     }
-}//Faculty
+}
